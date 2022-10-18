f62540df7fd1e35eaf854c4f277a8ef9c7d23bab
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-18 14:24:09 +0200

    add script to print list with for and while loop

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
+const shopList = [
+  "acqua",
+  "caffè",
+  "pasta",
+  "succo di arancia",
+  "ananas",
+  "melone",
+  "zucchero",
+  "sale",
+  "uova",
+  "farina 00",
+  "farina integrale"
+];
 
+// CICLO FOR
+for (let i = 0; i < shopList.length; i++) {
+  const listFor = document.querySelector(".output-ciclofor");
+  let listElement = shopList[i];
+  let li = document.createElement("li");
+  li.innerHTML = listElement;
+  listFor.append(li);
+}
+
+// CICLO WHILE
+let c = 0;
+while (c < shopList.length) {
+  const listWhile = document.querySelector(".output-ciclowhile");
+  let listElement = shopList[c];
+  let li = document.createElement("li");
+  li.innerHTML = listElement;
+  listWhile.append(li);
+  c++;
+}
+
+
+  
