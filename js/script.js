694c0a433f52d3cc95ae3b95f9d578d665234556
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-18 15:31:21 +0200

    add layout for while-loop list

--- js/script.js
+++ js/script.js
@@ -12,25 +12,28 @@ const shopList = [
   "farina integrale"
 ];
 
+const ulFor = document.createElement("ul");
+ulFor.className = "output ciclofor";
+document.querySelector(".lista.for").append(ulFor);
+
+const ulWhile = document.createElement("ul");
+ulWhile.className = "output ciclowhile";
+document.querySelector(".lista.while").append(ulWhile);
+
 // CICLO FOR
 for (let i = 0; i < shopList.length; i++) {
-  const listFor = document.querySelector(".output-ciclofor");
   let listElement = shopList[i];
   let li = document.createElement("li");
   li.innerHTML = listElement;
-  listFor.append(li);
+  ulFor.append(li);
 }
 
 // CICLO WHILE
 let c = 0;
 while (c < shopList.length) {
-  const listWhile = document.querySelector(".output-ciclowhile");
   let listElement = shopList[c];
   let li = document.createElement("li");
   li.innerHTML = listElement;
-  listWhile.append(li);
+  ulWhile.append(li);
   c++;
 }
-
-
-  
